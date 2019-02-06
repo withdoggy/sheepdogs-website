a4da3c0284adcda7e643b0435f24f47a50ad11cf
Wojciech Wójcik <wojtaswojcik@gmail.com>
2019-02-06 14:45:12 +0100

    add storybook and change router

---
+++ .storybook/config.js
@@ -0,0 +1,7 @@
+import { configure } from '@storybook/react';
+const req = require.context('../src/app/shared', true, /\.stories\.js$/);
+function loadStories() {
+  req.keys().forEach(filename => req(filename));
+}
+
+configure(loadStories, module);
